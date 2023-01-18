1253639baa3c2b4c9a9755f669ae83d8f7b6dfbc
trdxlucifer <singhdepu566@gmail.com>
2023-01-19 00:19:09 +0530

    changes in profile

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -11,10 +11,6 @@ import { doc, setDoc } from "firebase/firestore";
 const { TabPane } = Tabs;
 
 const Profile = () => {
-
-
- 
-
   return (
     <div className="container ">
       <div className="mt-5 space-x-24">
@@ -28,32 +24,23 @@ const Profile = () => {
           layout="vertical"
           onFinish={() => {}}
         >
-          <Tabs defaultActiveKey="2">
-            <TabPane aria-button="1"  tab="Personal Info" key='1'>
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Personal Info" key="1">
               <Personal />
             </TabPane>
-            <TabPane aria-button="2" tab="Skills and Education" key="2">
+            <TabPane tab="Skills and Education" key="2">
               <SkillsEducation />
             </TabPane>
-            <TabPane className="tab3 4"  tab="Experience / Projects" key="3">
+            <TabPane tab="Experience / Projects" key="3">
               <ExperienceProjects />
             </TabPane>
           </Tabs>
         </Form>
       </div>
       <div className="flex justify-around mt-4">
-        <div className=" mx-14 left-6">
-          {" "}
-          <button class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-            Prev
-          </button>
-        </div>
         <div className=" ">
           {" "}
-          <button
-            class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-            onClick={CHange}
-          >
+          <button class=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
             Next
           </button>
         </div>
